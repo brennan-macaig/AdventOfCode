f40eacd5b0de1db97fcdb4904a54b480601b7000
brennan-macaig <brennanm@santbani.org>
2016-11-05 23:43:54 -0400

    First commit. Day1

---
+++ src/com/brennanmacaig/advent/Day1.java
@@ -0,0 +1,39 @@
+package com.brennanmacaig.advent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Day1.java
+ * Written by Brennan Macaig for the Advent of Code 2015 Challenge
+ * Day 1's challenge can be found here: http://adventofcode.com/2015/day/1
+ * @author Brennan
+ *
+ */
+public class Day1 {
+	public static int i = 0;
+	public static int position = 1;
+	public static void main(String[] args) throws FileNotFoundException {
+		String puzzleInput = new Scanner(new File("inputs/day1")).useDelimiter("\\Z").next();
+		String[] puzzleArray = puzzleInput.split("");
+		
+		for(String s : puzzleArray) {
+			if(s.equalsIgnoreCase("(")) {
+				i++;
+			} else if(s.equalsIgnoreCase(")")) {
+				i--;
+			} else {
+				// won't happen.
+			}
+			
+			if (i == -1) {
+				// Position is here
+				System.out.println("Position: " + position);
+			}
+			position++;
+		}
+		
+		System.out.println("i = " + i);
+	}
+}
