d9de32cdff98fe8b1948b0e3cefb52137ca6deb2
brennan-macaig <brennanm@santbani.org>
2016-11-23 21:50:13 -0500

    Day 4 all done!

---
+++ src/com/brennanmacaig/advent/Day3.java
@@ -0,0 +1,135 @@
+package com.brennanmacaig.advent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Advent of Code day 3
+ * Programmed by Brennan Macaig
+ * You can find the challenge here: http://adventofcode.com/2015/day/3
+ */
+public class Day3 {
+	public static void main(String[] args) throws IOException {
+		String puzzleInput = new Scanner(new File("inputs/day3")).useDelimiter("\\Z").next();
+		String[] puzzleArray = puzzleInput.split("");
+		int xCounter = 0;
+		int yCounter = 0;
+		int xRobo = 0;
+		int yRobo = 0;
+		String pairs = "0,0";
+		String roboPairs = "0,0";
+		String coordPair = "";
+		String coordRobo = "";
+		for(int i = 0; i < puzzleArray.length; i++) {
+			String s = puzzleArray[i];
+			if((i == 0) || ((i%2) == 0)) {
+				// normal santa
+				switch(s) {
+					case "^":
+						yCounter++;
+						break;
+					case "v":
+						yCounter--;
+						break;
+					case ">":
+						xCounter++;
+						break;
+					case "<":
+						xCounter--;
+						break;
+					default:
+						System.out.println("Bad character this member... Skipping");
+						break;
+				}
+				coordPair = xCounter + "," + yCounter;
+				
+				String matchString = "";
+				boolean collision = false;
+				for(String check : pairs.split("#")) {
+					if(check.equals(coordPair)) {
+						// Found one!
+						matchString = "chk " + check + " pair: " + coordPair;
+						collision = true;
+						break;
+					}
+				}
+				if(!collision) {
+					pairs = pairs + "#" + coordPair;
+				}
+				System.out.println("NORMAL SANTA: " + coordPair + " Matched: " + collision + " " + matchString);
+				
+			} else {
+				// Robo-Santa
+				switch(s) {
+					case "^":
+						yRobo++;
+						break;
+					case "v":
+						yRobo--;
+						break;
+					case ">":
+						xRobo++;
+						break;
+					case "<":
+						xRobo--;
+						break;
+					default:
+						System.out.println("Bad character this member... Skipping");
+						break;
+				}
+				coordRobo = xRobo + "," + yRobo;
+				
+				String matchString = "";
+				boolean collision = false;
+				for(String check : roboPairs.split("#")) {
+					if(check.equals(coordRobo)) {
+						// Found one!
+						matchString = "chk " + check + " pair: " + coordRobo;
+						collision = true;
+						break;
+					}
+				}
+				if(!collision) {
+					roboPairs = roboPairs + "#" + coordRobo;
+				}
+				System.out.println("ROBO SANTA: " + coordRobo + " Matched: " + collision + " " + matchString);
+			} 
+
+		}
+		String finalAns="0,0";
+		String[] roboAns = roboPairs.split("#");
+		String[] normAns = pairs.split("#");
+		System.out.println(Arrays.toString(roboAns));
+		System.out.println(Arrays.toString(normAns));
+		String garbageMatch = "";
+		boolean finalCollision = false;
+		for (int i = 0; i < roboAns.length; i++) {
+			String s = roboAns[i];
+			for (int b = 0; b < normAns.length; b++) {
+				String c = normAns[b];
+				if(s.equals(c)) {
+					// OK. So S and C are equal to each other (they must be the same pair!)
+					// At this point we want to say that the array DID collide, and we SHOULDN'T match the pair into the new array.
+					// Otherwise, add the pair.
+					finalCollision = true;
+					System.out.println("Final collision at " + s + " other is: " + c);
+				}
+			}
+			if (!finalCollision) {
+				// Nothing matched
+				System.out.println("No match found for " + s);
+				if(s.equals("0,0")) {
+					// do nothing here
+				} else {
+					finalAns = finalAns + s;
+				}
+			}
+		}
+		finalAns = finalAns + "#" + pairs;
+		String[] blah = finalAns.split("#");
+		System.out.println(finalAns);
+		System.out.println("\nFinal ans: " + blah.length);
+	}
+}
---
+++ src/com/brennanmacaig/advent/Day4.java
@@ -0,0 +1,45 @@
+package com.brennanmacaig.advent;
+
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+/**
+ * Advent of Code day 4
+ * By Brennan Macaig
+ * Find the challenge here: http://adventofcode.com/2015/day/4
+ * 
+ * FUN FACT: This is the first challenge that doesn't read in from a file.
+ * See the day 4 file for more information.
+ */
+public class Day4 {
+	public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException { 
+		String puzzleInput = "iwrupvqb";
+		int counter = 0;
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		while(true) {
+			String text = puzzleInput + counter;
+			m.reset();
+			m.update(text.getBytes());
+			byte[] digest = m.digest();
+			BigInteger bigInt = new BigInteger(1,digest);
+			String hashtext = bigInt.toString(16);
+			while (hashtext.length() < 32) {
+				hashtext = "0"+hashtext;
+			}
+			System.out.println("Current counter: " + counter);
+			System.out.println("Current MD5: " + hashtext);
+			// For part two, just add a zero to the value there.
+			// I've left it at part 1's value for now.
+			if(hashtext.startsWith("00000")) {
+				System.out.println("\n=====================\n");
+				System.out.println("Verify MD5: " + hashtext);
+				System.out.println("Magic number: " + counter);
+				System.out.println("All done!");
+				break;
+			}
+			counter++;
+		}
+		
+	}
+}
